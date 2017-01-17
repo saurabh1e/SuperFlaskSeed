c31f6b8879da7b6c3a2304f470ace38a18216ec9
saurabh <saurabh.1e1@gmail.com>
2017-01-17 13:05:01 +0530

    removed unnecessary things

--- web/src/__init__.py
+++ web/src/__init__.py
@@ -3,5 +3,4 @@ from .utils import api, db, ma, create_app, ReprMixin, bp, BaseMixin, admin, Bas
 
 from .user import apiv1, models, schemas
 from .utils.security import security
-# from .utils.user_manager import user_manager
 from .admin_panel import admin_manager
