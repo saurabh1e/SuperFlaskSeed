f9ed8f60024cae7fc9c6ed0a4615b1ca53ad9b78
saurabh <saurabh.1e1@gmail.com>
2017-02-15 15:58:58 +0530

    operator updated for better code quality

--- web/src/utils/operators.py
+++ web/src/utils/operators.py
@@ -1,35 +1,87 @@
-class Operators(object):
-    op = 'exact'
+from abc import ABC, abstractstaticmethod
 
-    def prepare_queryset(self, query, model, key, value):
+
+class Operators(ABC):
+    op = 'equal'
+
+    @staticmethod
+    @abstractstaticmethod
+    def prepare_queryset(query, model, key, value):
         return query.filter(getattr(model, key) == value)
 
 
 class In(Operators):
     op = 'in'
 
-    def prepare_queryset(self,query, model, key, values):
+    @staticmethod
+    def prepare_queryset(query, model, key, values):
+        if len(values) == 1:
+            values = values[0].split(',')
         return query.filter(getattr(model, key).in_(values))
 
 
 class Equal(Operators):
-    op = 'exact'
+    op = 'equal'
 
-    def prepare_queryset(self, query, model, key, value):
+    @staticmethod
+    def prepare_queryset(query, model, key, value):
         return query.filter(getattr(model, key) == value[0])
 
 
 class Contains(Operators):
     op = 'contains'
 
-    def prepare_queryset(self, query, model, key, value):
+    @staticmethod
+    def prepare_queryset(query, model, key, value):
         return query.filter(getattr(model, key).contains(value[0]))
 
 
 class Boolean(Operators):
     op = 'bool'
 
-    def prepare_queryset(self, query, model, key, value):
+    @staticmethod
+    def prepare_queryset(query, model, key, value):
         val = False if value[0] == 'false' else True
         return query.filter(getattr(model, key) == val)
 
+
+class Between(Operators):
+    op = 'between'
+
+    @staticmethod
+    def prepare_queryset(query, model, key, value):
+        val1 = value[0]
+        val2 = value[1]
+        return query.filter(getattr(model, key).between(val1, val2))
+
+
+class Greater(Operators):
+    op = 'gt'
+
+    @staticmethod
+    def prepare_queryset(query, model, key, value):
+        return query.filter(getattr(model, key) > value[0])
+
+
+class Lesser(Operators):
+    op = 'lt'
+
+    @staticmethod
+    def prepare_queryset(query, model, key, value):
+        return query.filter(getattr(model, key) < value[0])
+
+
+class Greaterequal(Operators):
+    op = 'gte'
+
+    @staticmethod
+    def prepare_queryset(query, model, key, value):
+        return query.filter(getattr(model, key) >= value[0])
+
+
+class LesserEqual(Operators):
+    op = 'lte'
+
+    @staticmethod
+    def prepare_queryset(query, model, key, value):
+        return query.filter(getattr(model, key) <= value[0])
